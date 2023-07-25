9328c4bc997a17dcbaa6ca0fd102353638cd8281
Dominic Richard <dominicrichard@MacBook-Pro-3.local>
2023-07-24 18:59:56 -0600

    changed OrderGUI

--- src/view/OrderGUI.java
+++ src/view/OrderGUI.java
@@ -1,5 +1,14 @@
 package view;
 
-public class OrderGUI {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class OrderGUI extends Application {
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
